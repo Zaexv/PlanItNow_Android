7a7d586746ade825be80ed53b56354d6052c228f
Eduardo P <zaexv@hotmail.com>
2021-08-10 19:37:18 +0200

    [ADD] Create Plan Mutation [ADD] Create Plan View

--- app/src/main/java/com/planitnow/usecases/createplan/CreatePlanActivity.kt
+++ app/src/main/java/com/planitnow/usecases/createplan/CreatePlanActivity.kt
@@ -1,9 +1,14 @@
 package com.planitnow.usecases.createplan
 
+import android.app.TimePickerDialog
+import android.content.Context
 import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.graphql.models.CreatePlanMutation
+import com.planitnow.R
 import com.planitnow.databinding.ActivityCreatePlanBinding
-import com.planitnow.databinding.ActivityViewPlanBinding
 
 class CreatePlanActivity : AppCompatActivity() {
 
@@ -13,9 +18,26 @@ class CreatePlanActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityCreatePlanBinding.inflate(layoutInflater)
+
+        binding.viewCpInitHour.setOnClickListener(){
+            buildTimePicker(binding.viewCpInitHour)
+        }
+
+        binding.viewCpEndHour.setOnClickListener(){
+            buildTimePicker(binding.viewCpEndHour)
+        }
+
         setContentView(binding.root)
 
     }
 
+    private fun buildTimePicker(view : EditText){
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+            val timeString = "$hour:$minute"
+            view.setText(timeString)
+        }
+        TimePickerDialog(this, timeSetListener,4,20,true).show()
+
+    }
 
 }
